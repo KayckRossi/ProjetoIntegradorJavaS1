334b304fb5866b82aaff9aa02b7c69d306722a02
KayckRossi <kayck.12gabriel@gmail.com>
2023-12-01 16:30:59 -0300

    Signed-off-by: KayckRossi <kayck.12gabriel@gmail.com>

---
+++ TESTEEAD0/src/Circulo.java
@@ -0,0 +1,57 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC
+ */
+public class Circulo {
+    private double lerComprimento;
+
+    public double lerComprimento() {
+        System.out.println("Digite o comprimento do círculo: ");
+        Scanner input = new Scanner(System.in);
+        this.lerComprimento = input.nextDouble();
+        return this.lerComprimento;
+    }
+
+    public double getDiametro(double comprimento) {
+        double diametro = comprimento / 3.14;
+        return diametro;
+    }
+
+    public double getRaio(double diametro) {
+        double raio = diametro / 2;
+        return raio;
+    }
+
+    public double getArea(double raio) {
+        double area = raio * raio * 3.14;
+        return area;
+    }
+
+    public void plotar(double comprimento, double diametro, double raio, double area) {
+        System.out.println("Comprimento: " + comprimento);
+        System.out.println("Diâmetro: " + diametro);
+        System.out.println("Raio: " + raio);
+        System.out.println("Área: " + area);
+    }
+
+    public void controle() {
+        double comprimento = lerComprimento();
+        double diametro = getDiametro(comprimento);
+        double raio = getRaio(diametro);
+        double area = getArea(raio);
+        plotar(comprimento, diametro, raio, area);
+    }
+
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo();
+        circulo.controle();
+    }
+} 
---
+++ ado9/src/ado9/Ado9.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+public class Ado9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
